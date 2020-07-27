6dfd917e62b4680e40974c4316dd9c5fdb887405
sannilincoln <gbengaslc@gmail.com>
2020-07-27 22:06:53 +0100

    Updates

---
+++ precache-manifest.bdbc111634a030ea4636ff2f37b55546.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2c485a23456708060cfa006e4e914de",
+    "url": "/React-News-App/index.html"
+  },
+  {
+    "revision": "742967f81a3c0398a2a6",
+    "url": "/React-News-App/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "742967f81a3c0398a2a6",
+    "url": "/React-News-App/static/js/2.e19a0c7a.chunk.js"
+  },
+  {
+    "revision": "f25112c0081db71b0e5dd2ce64e909e6",
+    "url": "/React-News-App/static/js/2.e19a0c7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fc9f7f719390af7c979",
+    "url": "/React-News-App/static/js/main.07a57885.chunk.js"
+  },
+  {
+    "revision": "a088638f0a4869017a80",
+    "url": "/React-News-App/static/js/runtime-main.795a535a.js"
+  }
+]);
